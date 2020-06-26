2d61ef9afff9245c11f74677d8f404fa4636b8e7
ynFatal <63413763@qq.com>
2020-06-27 06:36:47 +0800

    edit print format

--- ribbon-provider-example/src/main/java/com/example/controller/ProviderController.java
+++ ribbon-provider-example/src/main/java/com/example/controller/ProviderController.java
@@ -9,14 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/6/25 0025 19:34
  */
 @RestController
-public class HelloController {
+public class ProviderController {
 
     @Value("${server.port}")
     private Integer port;
 
-    @GetMapping
-    public String hello() {
-        return "hello, have a good time! port: " + port;
+    @Value("${spring.application.name}")
+    private String name;
+
+    @GetMapping("/provider")
+    public String provider() {
+        return String.format("server name : %s, port: %s", name, port);
     }
 
 }
