46d6ce8a3698f07f900bc65f449fcb6307ed4030
ynFatal <63413763@qq.com>
2020-07-01 06:23:44 +0800

    add SpringClientFactory test

--- ribbon-consumer-example/src/main/java/com/example/controller/ConsumerController.java
+++ ribbon-consumer-example/src/main/java/com/example/controller/ConsumerController.java
@@ -1,6 +1,10 @@
 package com.example.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.IRule;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -9,11 +13,25 @@ import org.springframework.web.client.RestTemplate;
  * @author Fatal
  * @date 2020/6/25 0025 21:33
  */
+@Slf4j
 @RestController
+@AllArgsConstructor
 public class ConsumerController {
 
-    @Autowired
     private RestTemplate restTemplate;
+    private SpringClientFactory springClientFactory;
+
+    @GetMapping("/ribbon_context")
+    private SpringClientFactory ribbonContext() {
+        log.info("contextNames = [{}]", springClientFactory.getContextNames());
+        log.info("RIBBON-PROVIDER1-EXAMPLE's rule is [{}]", springClientFactory.getInstance("RIBBON-PROVIDER1-EXAMPLE", IRule.class));
+        log.info("RIBBON-PROVIDER1-EXAMPLE's ILoadBalancer is [{}]", springClientFactory.getInstance("RIBBON-PROVIDER1-EXAMPLE", ILoadBalancer.class));
+        log.info("RIBBON-PROVIDER2-EXAMPLE's rule is [{}]", springClientFactory.getInstance("RIBBON-PROVIDER2-EXAMPLE", IRule.class));
+        log.info("RIBBON-PROVIDER2-EXAMPLE's ILoadBalancer is [{}]", springClientFactory.getInstance("RIBBON-PROVIDER2-EXAMPLE", ILoadBalancer.class));
+        log.info("RIBBON-PROVIDER3-EXAMPLE's rule is [{}]", springClientFactory.getInstance("RIBBON-PROVIDER3-EXAMPLE", IRule.class));
+        log.info("RIBBON-PROVIDER3-EXAMPLE's ILoadBalancer is [{}]", springClientFactory.getInstance("RIBBON-PROVIDER3-EXAMPLE", ILoadBalancer.class));
+        return springClientFactory;
+    }
 
     @GetMapping("/provider1")
     public String provider1() {
